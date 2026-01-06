b7b975ba75ff941cbd64298d9d6293bcc9278dd7
rontu mohonto <newscenery28@gmail.com>
2026-01-06 20:44:13 +0600

    cor2

--- lib/controller/product.dart
+++ lib/controller/product.dart
@@ -4,16 +4,16 @@ import 'dart:developer';
 import 'package:http/http.dart' as http;
 
 class productservice {
-  Future<Map> getdata() async {
+  Future<List> getdata() async {
     try {
       Uri url = Uri.parse("https://eplay.coderangon.com/api/products");
       var response = await http.get(url);
       if (response.statusCode == 200) {
-        return jsonDecode(response.body);
+        return jsonDecode(response.body)['data'];
       }
     } catch (e) {
       log("=====Error : $e=====");
     }
-    return {};
+    return [];
   }
 }
