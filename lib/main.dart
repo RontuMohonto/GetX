28be52bbf2705782e67fcd9d83e8b63a393f843c
rontu mohonto <newscenery28@gmail.com>
2026-01-08 17:36:49 +0600

    tes14

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:studio_projects/view/Dada_screen/auth/UI.dart';
 
 import 'package:studio_projects/view/Test/ProductList/with_getx.dart';
 
@@ -13,9 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(debugShowCheckedModeBanner: false,
-    home: ProductLIst_getx(),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AuthScreen(),
     );
-
   }
 }
--- lib/view/Dada_screen/auth/UI.dart
+++ lib/view/Dada_screen/auth/UI.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:studio_projects/Dada_controller/widgets/text.dart';
 import 'package:studio_projects/view/Dada_screen/auth/reg/RegSc.dart';
 
 import '../../../Dada_controller/widgets/tabbar.dart';
@@ -18,18 +21,27 @@ class _AuthScreenState extends State<AuthScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: InkWell(
-          onTap: () {},
-          child: Image(image: AssetImage("assets/images/arrowback.png")),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFFF4F4F), Color(0xFFFF9A37)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
         ),
-        title: Image(image: AssetImage("assets/images/barimage.jpg")),
+        centerTitle: true,
+        title: CustomText(
+          text: "Authentication",
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
       ),
 
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
