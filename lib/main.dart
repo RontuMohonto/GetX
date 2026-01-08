a6c57edc35a37ac15b1d45e1e85f73227e862be5
rontu mohonto <newscenery28@gmail.com>
2026-01-08 23:51:50 +0600

    tes24

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:studio_projects/view/Dada_screen/auth/UI.dart';
+import 'package:studio_projects/view/Dada_screen/home/UI.dart';
 import 'package:studio_projects/view/Dada_screen/splash/UI.dart';
 
 import 'package:studio_projects/view/Test/ProductList/with_getx.dart';
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthScreen(),
+      home: home(),
     );
   }
 }
