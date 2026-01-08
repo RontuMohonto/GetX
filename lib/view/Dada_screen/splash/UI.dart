4015fdc3cfbc54065074951b20d9f8994fb928f1
rontu mohonto <newscenery28@gmail.com>
2026-01-08 21:51:48 +0600

    tes20

--- lib/view/Dada_screen/splash/UI.dart
+++ lib/view/Dada_screen/splash/UI.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get/get.dart';
+import 'package:studio_projects/controller/splashcontroller.dart';
 import '../ProductScreen/UI.dart';
 import '../auth/UI.dart';
 import '../home/UI.dart';
@@ -12,13 +14,7 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
-  //Splash Function
-
-  // @override
-  // void initState() {
-  //   TimerFunction();
-  //   super.initState();
-  // }
+  final SplashController sc = Get.put(SplashController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
